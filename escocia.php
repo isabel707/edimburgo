aab1f103cd83e87ca8ecc44cad6777a89d4cf262
isabel707 <80932336+isabel707@users.noreply.github.com>
2022-11-27 01:38:21 -0600

    Add files via upload

---
+++ escocia.php
@@ -0,0 +1,116 @@
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>Escocia</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="estilo.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+<header>Edimburgo, Escocia<img id="bandera"src="https://lh3.googleusercontent.com/5Yrkv7UC5kPm7Y07YEDA6k0Zwq09EuQlFALL70Mz4zsYVgdCosEWHSLmxtDxbNhOVTgJlWCjKVpJT1d9_WZyGjilGcKACZtt59Y2P8lL_A8E6-JPPHGH_lvTVxrSQDrTq4ozujDPPxE=w2400"></header>
+<br>
+<h1>Curiosidades sobre Edimburgo</h1>
+<br>
+<div class="container">
+<ol class="list-unstyled"></ol>
+<ol>
+    <h5>Haar, la niebla de Edimburgo</h5>
+    <li>Edimburgo tiene su propia niebla, llamada haar, que proviene de la costa. una bruma que se forma sobre el mar y que cubre la ciudad por varios días. Esta espesa niebla, 
+    mezclada con el humo de las chimeneas de carbón daba como resultado un cielo amarillo, llamado también Pea Soup.</li>
+    <br>
+    <h5>Auld Reekie</h5>
+    <li>En el pasado, Edimburgo era una ciudad con chimeneas humeantes que espesaban el aire y lo teñían de negro, haciendo que toda la ciudad oliera a humo.
+    Aunque la ciudad tiene un peculiar olor, no es a humo, sino a la mezcla de levadura y cebada, que proviene de las fábricas de cerveza.</li>
+    <br>
+    <h5>Puedes encontrar al menos de 5 restaurantes con estrellas Michelín</h5>
+    <li>No es tan fácil encontrar restaurantes que estén premiados con las prestigiosas estrellas Micheliín en Escocia, por ello nombraremos los 5 que pueden encontrar en Edimburgo:
+    “Number One” en Princes Street, “21212” en Royal Terrace and Castle Terrace, “Restaurant Martin Wishart” en The Shore y “The Kitchin” en Commercial Quay.</li>
+    <br>
+    <h5>El extraño corazón del Midlothian</h5>
+    <li>Junto a la Catedral de San Giles, en la Royal Mile, los visitantes pueden ver un corazón hecho con losas de granito sobre el pavimento. Es el lugar en el que se encontraba la antigua prisión de Edimburgo,
+     construida durante el siglo XV y demolida en 1817. Hoy en día es costumbre escupir en el interior del corazón para tener buena suerte, por lo que no es buena idea pasar por encima de él porque podríamos resbalar, aunque ésa no es la única razón por la que no es conveniente pisar el corazón: cuenta la leyenda que quien lo pisa, no tiene nunca suerte en el amor.</li>
+    <br>
+    <h5>El castillo de Edimburgo</h5>
+    <li>El Castillo de Edimburgo, en Castle Rock, es el monumento más importante y conocido de la ciudad. 
+    Está edificado sobre un volcán extinto y desde la última vez que entró en erupción han pasado más de 350 millones de años.</li>
+        <br>
+        <h5>Piedra del destino</h5>
+    <li>En el Castillo de Edimburgo se guarda la «Piedra del Destino». Es una piedra que tradicionalmente está presente en la coronación de la realeza escocesa e inglesa. Aunque existe una leyenda que dice que la actual Piedra del Destino no es la original, 
+    sino que fue cambiada por una falsificación en algún momento de la historia.</li>
+        <br>
+        <h5>Edimburgh Fringe Festival</h5>
+        <li>Es el festival artístico más grande del mundo. En principio fue un compendio de actos marginales del Festival Internacional de Edimburgo que tenía lugar durante el mes de agosto. 
+    Puedes ver cerca de 3.000 espectáculos en 300 escenarios y más de 50.000 representaciones. Acude gente de todo el mundo.</li>
+    <br>
+    <h5>Ciudad de la literatura</h5>
+    <li>Edimburgo fue la primera ciudad del mundo en ser nombrada por la UNESCO «Ciudad de la Literatura». Este ocurrió por su herencia y prestigio literario ya que autores de la talla de Conan Doyle, Robert Louis Stevenson, Muriel Spark, 
+    Irvine Welsh o Sir Walter Scott nacieron en ella. Eso sin contar que JK Rowling escribió el primer libro de la saga de Harry Potter en una cafetería de la ciudad.</li>
+    <br>
+    <h5>Haggis, el plato típico</h5>
+    <li>Un plato que elaboraban las familias más pobres con las partes de los animales que nadie quería: hígado, corazón, pulmones… 
+    todo ello embutido, condimentado y cocido durante varias horas.</li>
+    <br>
+    <h5>El cañón de la una</h5>
+    <li>En el Castillo de Edimburgo, todos los días, a la una del mediodía (excepto los domingos y otras festividades), tiene lugar una tradición que se lleva a cabo desde 1861. A la una, el General de Artillería dispara un moderno cañón, 
+    que perpetúa la tradición, pero que funciona sobre todo como reclamo turístico.</li>
+    </ol>
+</div>
+
+<div class="container" id="test">
+<br>
+<p><strong>Testimonios de visitantes</p></strong>
+<p><q><i>Pasé un mes increíble, muy buena experiencia. Hubo un intercambio cultural enorme, junto con actividades muy entretenidas de las que aprendí mucho.</i></q></p>
+<p><q><i>Es el mejor viaje que hice, fue una muy linda experiencia que repetiría mil veces. Todo estuvo organizado a nuestra comodidad y nos facilitó muchas cosas. 
+Lo pase muy bien y conocí un montón de gente y lugares lindos que hicieron de este viaje lo mejor. </i></q></p>
+<p><q><i>Me encantó este viaje,hicimos muchas actividades divertidas y los destinos fueron increíbles, 
+la residencia de Edimburgo fue perfecta y el vínculo que se armó con el grupo fue excelente, espero poder volver el año que viene.</i></q></p>
+<p><q><i>Este fue uno de los mejores viajes que hice. Me gusto mucho la experiencia y la volvería a repetir si se me diese la oportunidad.</i></q></P>
+<p><q><i>Edimburgo es una ciudad mágica, y la experiencia que recibimos todos también, es un viaje que no tiene descripción porque conoces un montón de personas 
+que sin ese viaje no hubieras conocido nunca y también te abre mucho a otras culturas que jamás hubieras conocido tan de cerca. Uno de los mejores viajes que podría haber hecho, me llene de amistades, risas y recuerdos que siempre van a quedar conmigo.</i></q></p>
+</div>
+
+<div class="slider-frame">
+  <ul>
+      <li><img src="https://lh3.googleusercontent.com/EbGatBAgWa8edwhAlFLJ15TsR59XCwMVa7nXBK2kBKHx1ImbdHTUrXtdluSGXUoQ_kibmlKoGz-94r0vRheRtJFIU6MhwFzIS1WqOXTWHIs5NQUhAhY4NDK2EBTfYn3QeyDESH3PExI=w2400" alt=""></li>
+      <li><img src="https://lh3.googleusercontent.com/0qlF0rfZ_STyP2ykYTuopBJfEnF682oRyS4B5OHw3G3EN72ZTg3_id7c85LOPIbQ1EJ_lOlwOxiaAb_zAadqOEKGs5HjPGitAismGhHgBYRi8rDPmjxnk3aJQPN887ybYAvmMLyYrnw=w2400" alt=""></li>
+      <li><img src="https://lh3.googleusercontent.com/f2KuD1xuIgPzaWJBUrPsX9vUf0B3Z3GauBMfd9oyk8GIzaesP1jVBPA-8XCOq23TSPmSXaRTK2rHsl64RxVr85szLYHpaS5ynlCxCerRt5fDMoHT6obwyIWlrCVhKVsAiaIjIBpEsog=w2400" alt=""></li>
+      <li><img src="https://lh3.googleusercontent.com/dPD-34ZKvbj6HFU-bVFrZTZOK9qft85HDjwbUPAFHu2b7cJqwIqtx81HXyrpPKDYgV4mtg4vJctLf2VJiFd9Apl5pUD580nosThJkdTp_ihDJFEKtc5PxutGOtiHgYUTKQPPHNKraBA=w2400" alt=""></li>
+      <li><img src="https://lh3.googleusercontent.com/j8d4ycGAiDzbo0B5P-VHPZiYUMVBQ-lTLulxYwqsTint77cfl8ZoB50tzwhDkhZDzoVfKnOlhssN0uS8jUd2tagvff_YJM9h6tytJ17uaDsnaUMebkcLEGv0ce2Xm5Za9WQLKLsLQ_w=w2400" alt=""></li>
+      <li><img src="https://lh3.googleusercontent.com/yi9UQLkBmsGtTj4OKRGf38BiC_vekm9H1IYQ0s7JPAVrdhxDbKLQh-pni0NnB8HJK53dG0orz0gdSHWQ6XR5JjRrBaF0dC50qfarIOKaILHztWKc6SUbT1AQUMoYBe3SCMdoaSrp0jU=w2400" alt=""></li>
+      <li><img src="https://lh3.googleusercontent.com/7xJEyUH27bE3a6z5RPNnCC7XU9MlOxaFwIwZmdukGk0MoMvpT937iEQd-2261S-v44MIA39hlPPd-kZmH-vrZVtEIjZcALFR7qr63eRqL0dIXaYY8jGPIU_pCkkSvJ2DMwvSXpz_nmk=w2400" alt=""></li>
+      <li><img src="https://lh3.googleusercontent.com/DxKzbbArM6VpWdCVQv4eF18Fn3wvvo21GmAMSkBxbg1E4-KmRyRgskMe_OxSlDsJEtiMDcUqJPbAISRMc_xZPLUdhzoDCOegcd6ePkWk1ALpFqMOZxIzmv5PImiLl5l9SGSh7_PaVMo=w2400" alt=""></li>
+      <li><img src="https://lh3.googleusercontent.com/3aAOkXaXo3NXghWFDMCZT-S83jrblB3CUzabuqgVp2zVxIfLC7QbbnCQnl0l2_hmuWZOiW7DNqk9hudbrgsrpyYMw2NYHEhGG0q75rOsj9YZQ0ZjqmdZGrv3VJiUby2agS8BnTRulJ0=w2400" alt=""></li>
+      <li><img src="https://lh3.googleusercontent.com/qgNm5y4TlcWPPnTE7zNlnRxw1aX0q-GB0709nSZV1eERuG9T-bGsZ5sasDRwM8FATFFmyWgLMPCYOq-zt0EboLX0B38QWrRCFSF6Slw_FOCRFbgoBK03NGz-Ff8-vsrKk5l0Z9mlGjI=w2400" alt=""></li>
+  </ul>
+</div>
+
+<h5 id="vid">Vídeos informativos</h5>
+<div id="videos">
+  <p></p>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/SRNyfBRhq0A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/Q5-xGGPUF4w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+
+<div class="container-sub">
+  <div class="content-sub">
+      <h2>Suscríbete</h2>
+      <p>Recibe actualizaciones sobre esta increíble cuidad</p>
+      <form action="#">
+          <input type="email" class="campo" placeholder="Introduce tu correo electrónico" required>
+          <input type="submit" value="Suscribir" class="btn-sub">
+      </form>
+      <br>
+      <br>
+      <p id="con"><strong>Contacto</strong></p>
+<ul id="cont">
+<li>Email: <a href="" id="email">fabi.gomezflores@gmail.com</a></li>
+<li>Código: <strong>220966254</li></strong>
+<li>Fabiola Isabel Gómez Flores</li>
+</ul>
+  </div>
+</div>
+</body>
+</html>
